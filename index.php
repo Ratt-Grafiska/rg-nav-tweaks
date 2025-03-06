8054332c8b8e4d3396dc769dca15c51766b71e4a
Johan <johan@rattgrafiska.se>
2025-03-06 17:40:06 +0100

    Bump version to 1.0.2 in index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ Plugin URI: https://github.com/Ratt-Grafiska/rg-nav-tweaks
 Update URI: https://github.com/Ratt-Grafiska/rg-nav-tweaks
 Description: This plugin improves the responsiveness and behavior of block-based navigation menus in WordPress. It dynamically injects CSS based on global theme layout settings, ensuring a seamless experience across different screen sizes. By tweaking menu visibility, open/close states, and responsiveness, it helps maintain better navigation control in block-based themes.
 
-Version: 1.1.1
+Version: 1.0.2
 Author: Johan Wistbacka
 Author URI: https://wistbacka.se
 License: GPL2
